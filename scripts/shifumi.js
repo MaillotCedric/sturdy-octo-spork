683bdcf64bf5e5ae0658ded14c5604c1e3f9ca69
MaillotCedric <cedricmaillot38@gmail.com>
2022-10-15 18:47:24 +0400

    Mise à jour script shifumi

--- scripts/shifumi.js
+++ scripts/shifumi.js
@@ -1,5 +1,15 @@
 // mon script
 
+let jeu = {
+    "choix_robot": "",
+    "mon_choix": "",
+    "figure_gagnante": "",
+    "figure_perdante": "",
+    "message": "",
+    "score_robot": 0,
+    "mon_score": 0
+};
+
 /**
  * 
  * @example
@@ -13,4 +23,54 @@ function figure_aleatoire() {
     let index_aleatoire = rand_pos_int(nb_figures - 1);
 
     return figures[index_aleatoire];
+};
+
+function partie_nulle(jeu) {
+    return jeu.choix_robot === jeu.mon_choix;
+};
+
+function robot_gagne(jeu) {
+    let choix_robot = jeu.choix_robot;
+    let mon_choix = jeu.mon_choix;
+
+    return  choix_robot === "pierre" ? (mon_choix === "lezard" || mon_choix === "ciseaux")
+        :   choix_robot === "feuille" ? (mon_choix === "pierre" || mon_choix === "spock")
+        :   choix_robot === "ciseaux" ? (mon_choix === "feuille" || mon_choix === "lezard")
+        :   choix_robot === "lezard" ? (mon_choix === "spock" || mon_choix === "feuille")
+        :   (mon_choix === "ciseaux" || mon_choix === "pierre"); // choix_robot === "spock"
+};
+
+function nouveau_message(jeu) {
+    let figure_gagnante = jeu.figure_gagnante;
+    let figure_perdante = jeu.figure_perdante;
+
+    return  figure_gagnante === "pierre" && figure_perdante === "lezard" ? "La pierre écrase le lézard"
+        :   figure_gagnante === "pierre" && figure_perdante === "ciseaux" ? "La pierre écrase les ciseaux"
+        :   figure_gagnante === "feuille" && figure_perdante === "pierre" ? "La feuille enveloppe la pierre"
+        :   figure_gagnante === "feuille" && figure_perdante === "spock" ? "La feuille désavoue Spock"
+        :   figure_gagnante === "ciseaux" && figure_perdante === "feuille" ? "Les ciseaux coupent la feuille"
+        :   figure_gagnante === "ciseaux" && figure_perdante === "lezard" ? "Les ciseaux décapitent le lézard"
+        :   figure_gagnante === "lezard" && figure_perdante === "spock" ? "Le lézard empoisonne Spock"
+        :   figure_gagnante === "lezard" && figure_perdante === "feuille" ? "Le lézard mange la feuille"
+        :   figure_gagnante === "spock" && figure_perdante === "ciseaux" ? "Spock écrase les ciseaux"
+        :   "Spock détruit la pierre"; // figure_gagnante === "spock" && figure_perdante === "pierre"
+};
+
+function nouvelle_partie(jeu) {
+    jeu.choix_robot = figure_aleatoire();
+    jeu.mon_choix = figure_aleatoire();
+
+    if (partie_nulle(jeu)) {
+        jeu.message = "Égalité";
+    } else if (robot_gagne(jeu)) {
+        jeu.figure_gagnante = jeu.choix_robot;
+        jeu.figure_perdante = jeu.mon_choix;
+        jeu.score_robot++;
+        jeu.message = nouveau_message(jeu);
+    } else { // je gagne
+        jeu.figure_gagnante = jeu.mon_choix;
+        jeu.figure_perdante = jeu.choix_robot;
+        jeu.mon_score++;
+        jeu.message = nouveau_message(jeu);
+    };
 };
